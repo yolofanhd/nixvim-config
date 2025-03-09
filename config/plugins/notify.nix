40d7dbfcc749e30b5f4ebaf3be98b537124f5326
yolofanhd <76175017+yolofanhd@users.noreply.github.com>
2025-03-09 14:54:32 +0100

    fix: fixed warnings

--- config/plugins/notify.nix
+++ config/plugins/notify.nix
@@ -2,11 +2,13 @@
   plugins = {
     notify = {
       enable = true;
-      backgroundColour = "#1e1e2e";
-      fps = 60;
-      render = "default";
-      timeout = 500;
-      topDown = false;
+      settings = {
+        backgroundColour = "#1e1e2e";
+        fps = 60;
+        render = "default";
+        timeout = 500;
+        topDown = false;
+      };
     };
   };
 }
