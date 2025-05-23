18c7307df53dc5bed8f5c068dd9dd2bf79784461
yolofanhd <76175017+yolofanhd@users.noreply.github.com>
2025-05-23 17:42:12 +0200

    Fix/warnings (#36)
    * fix: fixed warnings
    
    * feat: added basic lazy loading
    
    * feat: added snacks and auto-session, updated keybinds and readme

---
+++ config/plugins/snacks.nix
@@ -0,0 +1,19 @@
+{
+  plugins = {
+    snacks = {
+      enable = true;
+      settings = {
+        animate.enabled = false;
+        scroll.enabled = false;
+
+        bigfile.enabled = true;
+        image.enabled = true;
+        indent.enabled = true;
+        statuscolumn.enabled = true;
+        input.enabled = true;
+        scope.enabled = true;
+        quickfile.enabled = true;
+      };
+    };
+  };
+}
